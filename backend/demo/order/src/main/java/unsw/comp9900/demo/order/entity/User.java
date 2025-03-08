559a1dc86805fa2c8bb90284180c09cfe6b6ed7f
yoursweakfoe <42771668+yoursweakfoe@users.noreply.github.com>
2025-03-09 02:44:23 +1100

    entity

---
+++ backend/demo/order/src/main/java/unsw/comp9900/demo/order/entity/User.java
@@ -0,0 +1,11 @@
+package unsw.comp9900.demo.order.entity;
+
+import java.time.LocalDate;
+
+public class User {
+    private Integer id;
+    private String name;
+    private boolean gender;
+    private LocalDate birthday;
+
+}

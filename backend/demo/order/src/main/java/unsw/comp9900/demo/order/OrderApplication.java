15adf27ad9110faa0f15ce19898d56ba94e94235
yoursweakfoe <42771668+yoursweakfoe@users.noreply.github.com>
2025-03-09 02:27:45 +1100

    druid works

--- backend/demo/order/src/main/java/unsw/comp9900/demo/order/OrderApplication.java
+++ backend/demo/order/src/main/java/unsw/comp9900/demo/order/OrderApplication.java
@@ -1,5 +1,6 @@
 package unsw.comp9900.demo.order;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
@@ -10,6 +11,7 @@ import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@MapperScan("unsw.comp9900.demo.order.mapper")
 public class OrderApplication {
     public static void main(String[] args) {
         SpringApplication.run(OrderApplication.class, args);

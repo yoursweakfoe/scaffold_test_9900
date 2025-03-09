70c982363921de3a4aacf72a8dc52711d044df96
yoursweakfoe <42771668+yoursweakfoe@users.noreply.github.com>
2025-03-09 18:17:05 +1100

    mybatis plus service tested

--- backend/demo/order/src/main/java/unsw/comp9900/demo/order/service/impl/UserServiceImpl.java
+++ backend/demo/order/src/main/java/unsw/comp9900/demo/order/service/impl/UserServiceImpl.java
@@ -1,10 +1,12 @@
 package unsw.comp9900.demo.order.service.impl;
 
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
 import unsw.comp9900.demo.order.entity.User;
 import unsw.comp9900.demo.order.mapper.UserMapper;
 import unsw.comp9900.demo.order.service.UserService;
 
+@Service
 public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements UserService {
 
 }
--- backend/demo/order/src/test/java/unsw/comp9900/demo/order/test.java
+++ backend/demo/order/src/test/java/unsw/comp9900/demo/order/test.java
@@ -6,18 +6,23 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 import unsw.comp9900.demo.order.entity.User;
 import unsw.comp9900.demo.order.mapper.UserMapper;
+import unsw.comp9900.demo.order.service.UserService;
 
 import java.util.List;
 
 @SpringBootTest
 public class test {
+
     @Autowired
     private UserMapper userMapper;
 
+    @Autowired
+    private UserService userService;
+
     @Test
     public void testSelectList() {
-        List<User> list = userMapper.selectallUsertest();
-        list.forEach(System.out::println);
+        long count = userService.count();
+        System.out.println(count);
     }
 
 

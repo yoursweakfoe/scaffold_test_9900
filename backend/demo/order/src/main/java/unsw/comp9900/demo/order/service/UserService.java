3eb50e6bf309fc06e46d980fc3e3990401b0219f
yoursweakfoe <42771668+yoursweakfoe@users.noreply.github.com>
2025-03-09 17:53:10 +1100

    mybatis plus service tested

---
+++ backend/demo/order/src/main/java/unsw/comp9900/demo/order/service/UserService.java
@@ -0,0 +1,7 @@
+package unsw.comp9900.demo.order.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import unsw.comp9900.demo.order.entity.User;
+
+public interface UserService extends IService<User> {
+}
---
+++ backend/demo/order/src/main/java/unsw/comp9900/demo/order/service/impl/UserServiceImpl.java
@@ -0,0 +1,10 @@
+package unsw.comp9900.demo.order.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import unsw.comp9900.demo.order.entity.User;
+import unsw.comp9900.demo.order.mapper.UserMapper;
+import unsw.comp9900.demo.order.service.UserService;
+
+public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements UserService {
+
+}
